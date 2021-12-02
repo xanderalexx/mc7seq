458c6782a8a123e9027a1c1a463d24c4e5777ce6
xanderalex <lx16hd@gmail.com>
2021-12-02 01:52:52 +0000

    added config.py and .gitignore

---
+++ config/config.py
@@ -0,0 +1,3 @@
+ign = "xanderalex"
+ip = "192.168.1.15"
+password = "lmao"
--- mc.py
+++ mc.py
@@ -2,16 +2,18 @@ import RPi.GPIO as g
 from mcrcon import MCRcon as r
 import sevenseq.sevenseq as sevenseq
 import time
+import config.config as config
+if __name__ == '__main__':
+    import config.config as config
 
-with r('192.168.1.15', 'lmao') as mcr:
+with r(config.ip, config.password) as mcr:
     while True:
         time.sleep(0.01)
-        result = mcr.command("data get entity xanderalex Pos")
-        if "xanderalex has the following entity data: " in result:
-            raw = result.replace("xanderalex has the following entity data: ", "").replace(",", "").replace("[", "").replace("]", "").replace("d", "").split(" ")
+        result = mcr.command("data get entity " + config.ign + " Pos")
+        if (config.ign + " has the following entity data: ") in result:
+            raw = result.replace(config.ign + " has the following entity data: ", "").replace(",", "").replace("[", "").replace("]", "").replace("d", "").split(" ")
             if "-" in raw[1]:
-                rawneg = -abs(int(float(raw[1].replace("-", " "))))
-                rawneg = -abs(int(float(raw[1].replace("-", " "))))
+                rawneg = -abs(int(float(raw[1].replace("-", ""))))
                 sevenseq.setnum(rawneg)
             else:
                 sevenseq.setnum(int(float(raw[1])))
