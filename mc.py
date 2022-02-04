f7810d996c9536c47cda19996cd730ecc36e2f69
xanderalex <lx16hd@gmail.com>
2022-02-04 03:27:02 -0800

    added villager trades tracking

--- mc.py
+++ mc.py
@@ -10,7 +10,11 @@ if __name__ == '__main__':
 
 print(len(sys.argv), str(sys.argv))
 arg = sys.argv[1]
-
+try:
+    arg2 = sys.argv[2]
+except:
+    print("")
+#/data get entity @e[type=minecraft:villager, name=A1, limit=1] Offers.Recipes[5].uses
 with r(config.ip, config.password) as mcr:
     if arg == "y":
         while True:
@@ -29,3 +33,18 @@ with r(config.ip, config.password) as mcr:
             if (config.ign + " has the following entity data: ") in result:
                 raw = result.replace(config.ign + " has the following entity data: ", "").replace("f", "")
                 sevenseq.setnum(int(math.ceil(float(raw))))
+    elif arg == "villager":
+        if("," in arg2):
+            villagerarray = arg2.split(",")
+        else:
+            villagerarray = [arg2]
+        while True:
+            time.sleep(0.01)
+            total = 0
+            for v in villagerarray:
+                time.sleep(0.01)
+                result = mcr.command("data get entity @e[type=minecraft:villager, name=" + v + ", limit=1] Offers.Recipes[5].uses")
+                if(v + " has the following entity data: " in result):
+                    raw = result.replace(v + " has the following entity data: ", "")
+                    total = total + int(raw)
+            sevenseq.setnum(total)
