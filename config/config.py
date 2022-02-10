1db114cec7efba9dec8fb6784c2ea9ea03212b4c
xanderalex <lx16hd@gmail.com>
2022-02-10 15:17:50 -0800

    added config.py documentation

--- config/config.py
+++ config/config.py
@@ -1,3 +1,9 @@
-ign = "xanderalex"
-ip = "192.168.1.15"
-password = "lmao"
+ign = "xanderalex" #your in game name
+ip = "192.168.1.14" #ip of your server
+password = "lmao" #RCON password (set in server.properties)
+
+#for tracking villager trades availability. 
+#nametag your villagers and add them to the arrays below
+
+villagers_with_melons = ["A1", "A2", "A3", "A4"] 
+villagers_with_books = ["B1", "B3", "B5", "B7", "C2", "C4", "D1", "D2", "D3"]
